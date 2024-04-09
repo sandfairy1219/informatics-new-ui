93bc2dccfe391cb83af08ec7c65eeae4c54acb21
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-04-09 19:52:10 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function setInnerHTML()  {
+    const element = document.getElementById('container');
+    element.innerHTML = "<div>#include <stdio.h><br>main(){<br>int n, a;    *p.137 활동1*<br>scanf(%d%d, &n, &a);<br>printf(%d+%d=%d\n, n, a, n+a);<br>printf(%d-%d=%d\n, n, a, n-a);<br>printf(%d*%d=%d\n, n, a, n*a);<br>printf(%d/%d=%d\n, n, a, n/a);<br>printf(%d% %%d=%d\n, n, a, n%a);<br>int a, n;    *p.137 활동2*  <br>scanf(%d%d, &a, &n);  <br>printf(넒이 = %d, a*n/2);<br>int n ;p.137 활동3  <br>scanf(%d, &n);    <br>printf(넓이 = %1lf, n*n*3.14);<br>}</div>"}
+
+function setInnerHTML2()  {
+    const element = document.getElementById('container');
+    element.innerHTML = "<div>#include <stdio.h> <br>main() <br>{ <br>int a, b; <br>freopen(input.txt, r, stdin); <br>scanf(%d%d, &a, &b); <br>printf(%d\n, a+b);   <br>}   </div>";
+}  
