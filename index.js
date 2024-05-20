7fe8e2b41f569416eeaa62468ce18a97ea253866
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-05-20 17:47:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const btn4 = document.querySelector('#btn4');
 const btn5 = document.querySelector('#btn5');
 const btn6 = document.querySelector('#btn6');
 const btn7 = document.querySelector('#btn7');
-const btn8 = document.querySelector('#btn8');
 let toggle = document.querySelector('#toggle');
 btn1.addEventListener('click', function(){
     const isActive = btn1.classList.contains('active');
@@ -27,7 +26,7 @@ btn1.addEventListener('click', function(){
         btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h><br>main(){<br>int n, a;    *p.137 활동1*<br>scanf(%d%d, &n, &a);<br>printf(%d+%d=%d\n, n, a, n+a);<br>printf(%d-%d=%d\n, n, a, n-a);<br>printf(%d*%d=%d\n, n, a, n*a);<br>printf(%d/%d=%d\n, n, a, n/a);<br>printf(%d% %%d=%d\n, n, a, n%a);<br>int a, n;    *p.137 활동2*  <br>scanf(%d%d, &a, &n);  <br>printf(넒이 = %d, a*n/2);<br>int n ;p.137 활동3  <br>scanf(%d, &n);    <br>printf(넓이 = %1lf, n*n*3.14);<br>}</div>"
-
+    
     }
      else {
         btn1.classList.add('active');
@@ -63,7 +62,7 @@ btn2.addEventListener('click', function(){
         btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h> <br>main() <br>{ <br>int a, b; <br>freopen(input.txt, r, stdin); <br>scanf(%d%d, &a, &b); <br>printf(%d\n, a+b);   <br>}   </div>";
-
+    
     } else {
         btn2.classList.add('active');
         btn2.classList.remove('bluebutton')
@@ -98,7 +97,7 @@ btn2.classList.add('active');
 btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h> <br>main() <br>{<br>int c;  <br>float a;    <br>scanf(%d, &c);    <br>freopen(input.txt, r, stdin);   <br>scanf(%f, &a);    <br>printf(%lf\n, a*c*c); <br>}   <br></div>";
-
+    
     } else {
         btn3.classList.add('active');
         btn3.classList.remove('bluebutton')
@@ -134,7 +133,7 @@ btn6.classList.add('active');
 btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "#include <iostream> <br>main()  <br>                                 {<br>int n;												//p.144 실습1<br> scanf(%d, &n); <br>if(n>=10)<br>printf(성공);<br></br>int n;												//p.145 실습2 <br></br>scanf(%d, &n);<br></br>if(n>=10)<br></br>printf(성공);<br></br>if(n<10)<br></br>printf(실패); <br></br>int n; 												//p.146 실습3 <br></br>scanf(%d, &n);      				<br></br>				 if(n>=10)<br></br>printf(성공);<br></br>else<br></br>printf(실패);<br></br>    int n;												//p.146 실습4 % 응용 <br></br>scanf(%d, &n);<br></br>if(n>=80&&n<=100)<br></br>printf(성공);else if(n>100)<br></br>printf(오류입니다); <br></br>else <br></br>printf(실패);<br></br>        };"
-
+    
     } else {
         btn4.classList.add('active');
         btn4.classList.remove('bluebutton')
@@ -170,7 +169,7 @@ btn6.classList.add('active');
 btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "#include <iostream> <br>main()  <br>                                 {<br> char n;<br></br>scanf(%c, &n);<br></br>if(n == 'A')<br></br>printf(good good);<br></br>else if(n == 'B')<br></br>printf(good);<br></br>else if(n == 'C')<br></br>printf(god);<br></br>else<br></br>printf(oh my god);<br></br>}"
-
+    
     } else {
         btn5.classList.add('active');
         btn5.classList.remove('bluebutton')
@@ -206,7 +205,7 @@ btn5.classList.add('active');
 btn7.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = " #include <iostream> <br>main(){<br>int n;                               // p.147 활동 1 <br> scanf(%d, &n);<br> if(n%2 == 0)<br>  printf(짝수입니다);<br>   else<br>  printf(홀수입니다);        <br>   int n;							  	// P.148 활동2 <br>   scanf(%d, &n);<br>   if(n ==2)<br>   printf(바위);<br>   else if(n==3)<br>   printf(보); <br>  else if (n==1)<br>  printf(가위);<br>   else <br>  printf(다시 입력);<br>   int n;                             // p.149 실습5 <br>   scanf(%d, &n)<br>   if(n>=90)<br>    printf(A);<br>    if (n<90 && n>=80)<br>    printf(B);<br>  if (n<80 && n>=70)<br>   printf(C);<br>   if(n<70)<br>     printf(F)<br>   int n;                            // p.150 실습6 <br>   scanf(%d, &n)<br>   if(n>=90)<br>   printf(A);<br>   else if (n>=80)<br>   printf(B);<br>   else if(n>=70)<br>   printf(C);<br>  else <br>  printf(F);<br>   }"
-
+    
     } else {
         btn6.classList.add('active');
         btn6.classList.remove('bluebutton')
@@ -274,9 +273,10 @@ cat.addEventListener('click',function(){
 })
 const line = document.querySelector('#line')
 let container = document.querySelector('#container')
-function lc(){
+line.addEventListener('click', function(){
     if(container.style.border !== 'none'){
         container.style.border =  'none';
     }else{
         container.style.border = '0.05cm solid black';
     }
+})
