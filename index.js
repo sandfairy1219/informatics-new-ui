dc642580b32aaf654cb05306bd077757eaafe571
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-05-20 13:42:17 +0900

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const btn1 = document.querySelector('#btn1');
+const btn1 = document.querySelector('#btn1'); // html문서에서 버튼 변수로 지정하기
 const btn2 = document.querySelector('#btn2');
 const btn3 = document.querySelector('#btn3');
 const btn4 = document.querySelector('#btn4');
 const btn5 = document.querySelector('#btn5');
 const btn6 = document.querySelector('#btn6');
 const btn7 = document.querySelector('#btn7');
+const btn8 = document.querySelector('#btn8');
 let toggle = document.querySelector('#toggle');
-btn1.addEventListener('click', function(){
-    const isActive = btn1.classList.contains('active');
+btn1.addEventListener('click', function(){  // 자신의 id로 addeventlistener 만들기
+    const isActive = btn1.classList.contains('active'); //자신의 id 쓰기
     if (isActive) {
-        btn1.classList.remove('active');
-        btn2.classList.remove('bluebutton')
+        btn1.classList.remove('active'); //자신의 active 클래스 지우기
+        btn2.classList.remove('bluebutton') // 자신을 제외한 다른 버튼들의 bluebutton 클래스 지우기
         btn3.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
-        btn1.classList.add('bluebutton')
-        btn2.classList.add('active');
+        btn1.classList.add('bluebutton') // 자신만 bluebutton 클래스 추가하기
+        btn2.classList.add('active'); // 자신을 제외한 다른 버튼들의 active 클래스 지우기
         btn3.classList.add('active');
         btn4.classList.add('active');
         btn5.classList.add('active');
         btn6.classList.add('active');
         btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h><br>main(){<br>int n, a;    *p.137 활동1*<br>scanf(%d%d, &n, &a);<br>printf(%d+%d=%d\n, n, a, n+a);<br>printf(%d-%d=%d\n, n, a, n-a);<br>printf(%d*%d=%d\n, n, a, n*a);<br>printf(%d/%d=%d\n, n, a, n/a);<br>printf(%d% %%d=%d\n, n, a, n%a);<br>int a, n;    *p.137 활동2*  <br>scanf(%d%d, &a, &n);  <br>printf(넒이 = %d, a*n/2);<br>int n ;p.137 활동3  <br>scanf(%d, &n);    <br>printf(넓이 = %1lf, n*n*3.14);<br>}</div>"
     
     }
      else {
-        btn1.classList.add('active');
-        btn1.classList.remove('bluebutton')
+        btn1.classList.add('active'); // 자신에게만 active 클래스 추가하기
+        btn1.classList.remove('bluebutton') // 모든 버튼의 bluebutton 클래스 지우기
         btn2.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+        btn8.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
@@ -49,7 +51,7 @@ btn2.addEventListener('click', function(){
         btn1.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
@@ -60,6 +62,7 @@ btn2.addEventListener('click', function(){
         btn5.classList.add('active');
         btn6.classList.add('active');
         btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h> <br>main() <br>{ <br>int a, b; <br>freopen(input.txt, r, stdin); <br>scanf(%d%d, &a, &b); <br>printf(%d\n, a+b);   <br>}   </div>";
     
@@ -70,7 +73,7 @@ btn2.addEventListener('click', function(){
         btn3.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+  btn8.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
         const element = document.getElementById('container');
@@ -85,7 +88,7 @@ btn3.addEventListener('click', function(){
         btn1.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
         btn3.classList.add('bluebutton')
@@ -95,6 +98,7 @@ btn5.classList.add('active');
 btn6.classList.add('active');
 btn2.classList.add('active');
 btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "<div>#include <stdio.h> <br>main() <br>{<br>int c;  <br>float a;    <br>scanf(%d, &c);    <br>freopen(input.txt, r, stdin);   <br>scanf(%f, &a);    <br>printf(%lf\n, a*c*c); <br>}   <br></div>";
     
@@ -104,7 +108,7 @@ btn7.classList.add('active');
         btn2.classList.remove('bluebutton')
         btn1.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+  btn8.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
@@ -119,7 +123,7 @@ btn4.addEventListener('click', function(){
         btn2.classList.remove('bluebutton')
         btn1.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
@@ -131,6 +135,7 @@ btn3.classList.add('active');
 btn5.classList.add('active');
 btn6.classList.add('active');
 btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "#include <iostream> <br>main()  <br>                                 {<br>int n;												//p.144 실습1<br> scanf(%d, &n); <br>if(n>=10)<br>printf(성공);<br></br>int n;												//p.145 실습2 <br></br>scanf(%d, &n);<br></br>if(n>=10)<br></br>printf(성공);<br></br>if(n<10)<br></br>printf(실패); <br></br>int n; 												//p.146 실습3 <br></br>scanf(%d, &n);      				<br></br>				 if(n>=10)<br></br>printf(성공);<br></br>else<br></br>printf(실패);<br></br>    int n;												//p.146 실습4 % 응용 <br></br>scanf(%d, &n);<br></br>if(n>=80&&n<=100)<br></br>printf(성공);else if(n>100)<br></br>printf(오류입니다); <br></br>else <br></br>printf(실패);<br></br>        };"
     
@@ -141,7 +146,7 @@ btn7.classList.add('active');
         btn1.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+  btn8.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
         const element = document.getElementById('container');
@@ -157,7 +162,7 @@ btn5.addEventListener('click', function(){
         btn4.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
         btn5.classList.add('bluebutton')
         btn1.classList.add('active');
@@ -167,6 +172,7 @@ btn4.classList.add('active');
 
 btn6.classList.add('active');
 btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = "#include <iostream> <br>main()  <br>                                 {<br> char n;<br></br>scanf(%c, &n);<br></br>if(n == 'A')<br></br>printf(good good);<br></br>else if(n == 'B')<br></br>printf(good);<br></br>else if(n == 'C')<br></br>printf(god);<br></br>else<br></br>printf(oh my god);<br></br>}"
     
@@ -176,7 +182,7 @@ btn7.classList.add('active');
         btn2.classList.remove('bluebutton')
         btn1.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+  btn8.classList.remove('bluebutton')
         btn4.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
@@ -194,7 +200,7 @@ btn6.addEventListener('click', function(){
         btn5.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn7.classList.remove('bluebutton')
-
+btn8.classList.remove('bluebutton')
         btn6.classList.add('bluebutton')
         btn1.classList.add('active');
         btn2.classList.add('active');
@@ -203,6 +209,7 @@ btn4.classList.add('active');
 btn5.classList.add('active');
 
 btn7.classList.add('active');
+         btn8.classList.add('active');
         const element = document.getElementById('container');
         element.innerHTML = " #include <iostream> <br>main(){<br>int n;                               // p.147 활동 1 <br> scanf(%d, &n);<br> if(n%2 == 0)<br>  printf(짝수입니다);<br>   else<br>  printf(홀수입니다);        <br>   int n;							  	// P.148 활동2 <br>   scanf(%d, &n);<br>   if(n ==2)<br>   printf(바위);<br>   else if(n==3)<br>   printf(보); <br>  else if (n==1)<br>  printf(가위);<br>   else <br>  printf(다시 입력);<br>   int n;                             // p.149 실습5 <br>   scanf(%d, &n)<br>   if(n>=90)<br>    printf(A);<br>    if (n<90 && n>=80)<br>    printf(B);<br>  if (n<80 && n>=70)<br>   printf(C);<br>   if(n<70)<br>     printf(F)<br>   int n;                            // p.150 실습6 <br>   scanf(%d, &n)<br>   if(n>=90)<br>   printf(A);<br>   else if (n>=80)<br>   printf(B);<br>   else if(n>=70)<br>   printf(C);<br>  else <br>  printf(F);<br>   }"
     
@@ -214,7 +221,7 @@ btn7.classList.add('active');
         btn4.classList.remove('bluebutton')
         btn5.classList.remove('bluebutton')      
         btn7.classList.remove('bluebutton')
-
+  btn8.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         const element = document.getElementById('container');
         element.innerHTML = ""
@@ -230,6 +237,7 @@ btn7.addEventListener('click', function(){
         btn5.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
+        btn8.classList.remove('bluebutton')
         btn7.classList.add('bluebutton')
         btn1.classList.add('active');
         btn2.classList.add('active');
@@ -237,6 +245,7 @@ btn3.classList.add('active');
 btn4.classList.add('active');
 btn5.classList.add('active');
 btn6.classList.add('active');
+         btn8.classList.add('active');
 
         const element = document.getElementById('container');
         element.innerHTML = "#include <iostream><br>main()<br>{<br> int n ;<br>  scanf(%d, &n);<br>    if(n>=3&&n<=5)<br>   printf(봄);<br>  else if(n>=6&&n<=8)<br>  printf(여름);<br>  else if(n>=9 &&n<=11)<br>  printf(가을);<br> else<br> printf(겨울);<br>}"
@@ -249,10 +258,47 @@ btn6.classList.add('active');
         btn5.classList.remove('bluebutton')
         btn3.classList.remove('bluebutton')
         btn6.classList.remove('bluebutton')
+          btn8.classList.remove('bluebutton')
         const element = document.getElementById('container');
         element.innerHTML = ""
     }
 })  
+btn8.addEventListener('click', function(){
+    const isActive = btn8.classList.contains('active');
+    if (isActive) {
+        btn8.classList.remove('active');
+        btn2.classList.remove('bluebutton')
+        btn1.classList.remove('bluebutton')
+        btn4.classList.remove('bluebutton')
+        btn5.classList.remove('bluebutton')
+        btn3.classList.remove('bluebutton')
+        btn6.classList.remove('bluebutton')
+        btn7.classList.remove('bluebutton')
+        btn8.classList.add('bluebutton')
+        btn1.classList.add('active');
+        btn2.classList.add('active');
+btn3.classList.add('active');
+btn4.classList.add('active');
+btn5.classList.add('active');
+btn6.classList.add('active');
+        btn7.classList.add('active');
+
+        const element = document.getElementById('container');
+        element.innerHTML = "미완성"
+    } else {
+        btn8.classList.add('active');
+        btn8.classList.remove('bluebutton')
+        btn2.classList.remove('bluebutton')
+        btn1.classList.remove('bluebutton')
+        btn4.classList.remove('bluebutton')
+        btn5.classList.remove('bluebutton')
+        btn3.classList.remove('bluebutton')
+        btn6.classList.remove('bluebutton')
+        btn7.classList.remove('bluebutton')
+        const element = document.getElementById('container');
+        element.innerHTML = ""
+    }
+}) 
 const rawcode = document.querySelector('#rawcode')
 rawcode.addEventListener('click', function(){
     location.href = 'https://github.com/sandfairy1219/informatics-new-ui'
